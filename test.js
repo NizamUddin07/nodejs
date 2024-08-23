48fda9403fe5f15de0acd307f0f506ec991388e8
MD NIZAM UDDIN <53169290+NizamUddin07@users.noreply.github.com>
2024-08-23 21:31:35 +0600

    find index number

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
-var ricePrice = 70;
-var onionPirce = 80;
+// var ricePrice = 70;
+// var onionPirce = 80;
 
-var totalCost = ricePrice + onionPirce;
+// var totalCost = ricePrice + onionPirce;
 
-console.log(totalCost);
+// console.log(totalCost);
+
+// var numbers=[45,68,44,55,69];
+// var element = numbers[1];
+// console.log(element);
+
+var numbers =[45,58,69,35,78]
+var positionIndex = numbers.indexOf(35);
+console.log(positionIndex);
